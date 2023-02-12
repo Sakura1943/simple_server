1480649a96daea698907712cecaeedef8513c93f
Sakura1943 <1436700265@qq.com>
2023-02-12 13:47:02 +0800

    Update

---
+++ completions/simple_server.bash
@@ -0,0 +1,70 @@
+_simple_server() {
+    local i cur prev opts cmd
+    COMPREPLY=()
+    cur="${COMP_WORDS[COMP_CWORD]}"
+    prev="${COMP_WORDS[COMP_CWORD-1]}"
+    cmd=""
+    opts=""
+
+    for i in ${COMP_WORDS[@]}
+    do
+        case "${cmd},${i}" in
+            ",$1")
+                cmd="simple_server"
+                ;;
+            *)
+                ;;
+        esac
+    done
+
+    case "${cmd}" in
+        simple_server)
+            opts="-p -b -d -i -h -V --port --bind --dir --index-file --help --version"
+            if [[ ${cur} == -* || ${COMP_CWORD} -eq 1 ]] ; then
+                COMPREPLY=( $(compgen -W "${opts}" -- "${cur}") )
+                return 0
+            fi
+            case "${prev}" in
+                --port)
+                    COMPREPLY=($(compgen -f "${cur}"))
+                    return 0
+                    ;;
+                -p)
+                    COMPREPLY=($(compgen -f "${cur}"))
+                    return 0
+                    ;;
+                --bind)
+                    COMPREPLY=($(compgen -f "${cur}"))
+                    return 0
+                    ;;
+                -b)
+                    COMPREPLY=($(compgen -f "${cur}"))
+                    return 0
+                    ;;
+                --dir)
+                    COMPREPLY=($(compgen -f "${cur}"))
+                    return 0
+                    ;;
+                -d)
+                    COMPREPLY=($(compgen -f "${cur}"))
+                    return 0
+                    ;;
+                --index-file)
+                    COMPREPLY=($(compgen -f "${cur}"))
+                    return 0
+                    ;;
+                -i)
+                    COMPREPLY=($(compgen -f "${cur}"))
+                    return 0
+                    ;;
+                *)
+                    COMPREPLY=()
+                    ;;
+            esac
+            COMPREPLY=( $(compgen -W "${opts}" -- "${cur}") )
+            return 0
+            ;;
+    esac
+}
+
+complete -F _simple_server -o bashdefault -o default simple_server
---
+++ completions/simple_server.fish
@@ -0,0 +1,6 @@
+complete -c simple_server -s p -l port -d 'Server port' -r
+complete -c simple_server -s b -l bind -d 'Server bind address' -r
+complete -c simple_server -s d -l dir -d 'Server bind directory' -r -F
+complete -c simple_server -s i -l index-file -d 'Server index file' -r
+complete -c simple_server -s h -l help -d 'Print help'
+complete -c simple_server -s V -l version -d 'Print version'
